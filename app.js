95fcf6a808872e9ce9291dbe62cc137275070e2e
JusTerrell <54183992+JusTerrell@users.noreply.github.com>
2019-12-27 13:07:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// my age
+const myAge = 25
+// early yaers
+let earlyYears = 2
+earlyYears *= 10.5
+// laterYears
+let laterYears = myAge - 2
+// dog years equation
+laterYears *= 4
+console.log(earlyYears)
+console.log(laterYears)
+// dog years variable
+let myAgeInDogYears = earlyYears + laterYears
+// lower case name
+let myName = 'Terrell'.toLowerCase()
+// Statement
+console.log(`My name is ${myName}. I am ${myAge} in human years which is ${myAgeInDogYears} years old in dog years.`);
